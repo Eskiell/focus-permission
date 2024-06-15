67c86d7dfb1ac40a84909c51bc2690ded10a5816
Eskiell <56792141+Eskiell@users.noreply.github.com>
2024-06-15 12:34:34 -0300

    Update FocusPermissionServiceProvider.php

--- src/FocusPermissionServiceProvider.php
+++ src/FocusPermissionServiceProvider.php
@@ -11,11 +11,21 @@ class FocusPermissionServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Carregar as migrations do pacote
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
         if ($this->app->runningInConsole()) {
+            // Publicar o arquivo de configuração
             $this->publishes([
                 __DIR__ . '/../config/permission.php' => config_path('focus-permission.php'),
             ], 'config');
+
+            // Publicar as migrations
+            $this->publishes([
+                __DIR__ . '/../database/migrations' => database_path('migrations'),
+            ], 'migrations');
+
+            // Registrar comandos de console, se houver
             $this->commands([
                 Commands\PermissionsGenerate::class
             ]);
@@ -27,7 +37,10 @@ class FocusPermissionServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Mesclar configurações
         $this->mergeConfigFrom(__DIR__ . '/../config/permission.php', 'focus-permission');
+        
+        // Registrar um singleton no container de serviço
         $this->app->singleton('focus-permission', function () {
             return new FocusPermission;
         });
